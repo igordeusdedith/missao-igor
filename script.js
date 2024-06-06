514e6c24ac6f1eafc03f311a672d6ba57af1cf1a
igordeusdedith <105360662+igordeusdedith@users.noreply.github.com>
2024-06-06 08:43:27 -0300

    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+ const caixaPrincipal = document.querySelector(".caixa-principal");
+ const caixPerguntas = document.querySelector(".caixa-pergunta");
+ const caixaAlternativa = document.querySelector(".caixa-alternativa");
+ const caixaResultado = document.querySelector(".caixa-resultado");
+ const textoResultado = document.querySelector(".texto-resultado");
+
+ const perguntas = [
+    { //primeiro objeto da lista de perguntas
+        enunciado:"pergunta1"
+        alternativa: ["alternativa1","alternativa2"]
+    },
+    { //sgundo objeto da lista de perguntas 
+        enunciado "pergunta2 ",
+        alternativa:["alternativa","alternativa2"]
+
+    }
+ ];
+
+ 
+ 
+
+ 
